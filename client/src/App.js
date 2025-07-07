19a066a251a29d776a458e2fbfd3b3eef09bf148
eun22-ui <haeun19@naver.com>
2025-07-07 18:59:06 +0900

    aaa

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import ImportComponent from './ImportComponent';
+import LifecycleTest from './LifecycleTest';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* <div>
+        <ImportComponent></ImportComponent>
+        <LifecycleTest>
+          prop_val = 'from data';
+        </LifecycleTest>
+      </div> */}
+      
+      <HousingFinder />
+
+
     </div>
   );
 }
